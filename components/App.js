d59604557ca3a66a447cf6fd29ffcb8381c10f6d
Romain Durand Saint Omer <romain.dso@gmail.com>
2016-07-26 21:50:21 +0200

    Extract App component

---
+++ components/App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Navigator
+} from 'react-native';
+import Card from './Card.js';
+import CardsList from './CardsList.js';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      currentCard: ''
+    };
+  }
+
+  onPressCard(card, navigator){
+    this.setState({currentCard: card});
+    this.navigate(navigator, 'Card');
+  }
+
+  navigate(navigator, name, type='Normal'){
+    navigator.push({
+      name,
+      type
+    });
+  }
+
+  renderScene(route, navigator){
+  	if(route.name == 'CardsList'){
+    	return (
+        <CardsList navigator={navigator} navigate={this.navigate} pressCardHandler={this.onPressCard.bind(this)}/>
+      )
+    } else if(route.name == 'Card'){
+      return (
+        <Card currentCard={this.state.currentCard} navigator={navigator} navigate={this.navigate} />
+      )
+    } else {
+      this.navigate(navigator, 'CardsList', 'Modal');
+    }
+  }
+
+  configureScene(route, routeStack){
+    if(route.type === 'Modal') {
+      return Navigator.SceneConfigs.FloatFromBottom;
+    }
+    return Navigator.SceneConfigs.PushFromRight;
+  }
+
+  render(){
+    return (
+      <Navigator
+      	style={{ flex:1 }}
+        initialRoute={{ name: 'CardsList' }}
+        renderScene={this.renderScene.bind(this)}
+        configureScene={this.configureScene.bind(this)}
+      />
+    )
+  }
+}
+
+export default App;
--- index.ios.js
+++ index.ios.js
@@ -4,70 +4,25 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  Navigator
-} from 'react-native';
-import Card from './components/Card.js';
-import CardsList from './components/CardsList.js';
+ /**
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  * @flow
+  */
 
-class ScrumPoker extends Component {
+ import React, { Component } from 'react';
+ import {
+   AppRegistry
+ } from 'react-native';
+ import App from './components/App.js';
 
-  constructor(props){
-    super(props);
-    this.state = {
-      currentCard: ''
-    };
-  }
+ class ScrumPoker extends Component {
 
-  onPressCard(card, navigator){
-    this.setState({currentCard: card})
-    this.navigate(navigator, 'Card');
-  }
-
-  navigate(navigator, name, type='Normal'){
-    navigator.push({
-      name,
-      type
-    });
-  }
-
-  renderScene(route, navigator){
-  	if(route.name == 'CardsList'){
-    	return (
-        <CardsList navigator={navigator} navigate={this.navigate} pressCardHandler={this.onPressCard.bind(this)}/>
-      )
-    } else if(route.name == 'Card'){
-      return (
-        <Card currentCard={this.state.currentCard} navigator={navigator} navigate={this.navigate} />
-      )
-    } else {
-      this.navigate(navigator, 'CardsList', 'Modal')
-    }
-  }
-
-  configureScene(route, routeStack){
-    if(route.type === 'Modal') {
-      return Navigator.SceneConfigs.FloatFromBottom
-    }
-    return Navigator.SceneConfigs.PushFromRight
-  }
-
-  render(){
-    return (
-      <Navigator
-      	style={{ flex:1 }}
-        initialRoute={{ name: 'CardsList' }}
-        renderScene={this.renderScene.bind(this)}
-        configureScene={this.configureScene.bind(this)}
-      />
-    )
-  }
-}
+   render(){
+     return (
+       <App />
+     )
+   }
+ }
 
 AppRegistry.registerComponent('ScrumPoker', () => ScrumPoker);
